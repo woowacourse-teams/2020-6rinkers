a1adbc0db8512aebe0efb4f6de512d674d28741d
kuenhwi <kueniyam@naver.com>
2020-07-14 17:34:04 +0900

    feat: 칵테일 단일 조회를 위한 서비스 구현

--- back/src/main/java/com/cocktailpick/back/cocktail/service/CocktailService.java
+++ back/src/main/java/com/cocktailpick/back/cocktail/service/CocktailService.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cocktailpick.back.cocktail.domain.Cocktail;
+import com.cocktailpick.back.cocktail.dto.CocktailDetailResponse;
 import com.cocktailpick.back.cocktail.dto.CocktailResponse;
 import com.cocktailpick.back.cocktail.repository.CocktailRepository;
 
@@ -23,5 +25,13 @@ public class CocktailService {
 		return Collections.unmodifiableList(cocktailRepository.findAll().stream()
 			.map(CocktailResponse::of)
 			.collect(Collectors.toList()));
+	}
+
+	@Transactional(readOnly = true)
+	public CocktailDetailResponse findCocktail(Long id) {
+		Cocktail cocktail = cocktailRepository.findById(id)
+			.orElseThrow(RuntimeException::new);
+
+		return CocktailDetailResponse.of(cocktail);
 	}
 }
--- back/src/test/java/com/cocktailpick/back/cocktail/service/CocktailServiceTest.java
+++ back/src/test/java/com/cocktailpick/back/cocktail/service/CocktailServiceTest.java
@@ -1,10 +1,12 @@
 package com.cocktailpick.back.cocktail.service;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +16,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.cocktailpick.back.cocktail.domain.Cocktail;
+import com.cocktailpick.back.cocktail.domain.Flavor;
+import com.cocktailpick.back.cocktail.dto.CocktailDetailResponse;
 import com.cocktailpick.back.cocktail.dto.CocktailResponse;
 import com.cocktailpick.back.cocktail.repository.CocktailRepository;
 
@@ -46,5 +50,49 @@ class CocktailServiceTest {
 
 		assertThat(cocktails).extracting("name")
 			.containsExactly(blueHawaii.getName(), martini.getName());
+	}
+
+	@DisplayName("단일 칵테일을 조회한다.")
+	@Test
+	void findCocktail() {
+		Flavor flavor = Flavor.builder()
+			.bitter(true)
+			.sour(true)
+			.sweet(false)
+			.build();
+
+		Cocktail blueHawaii = Cocktail.builder()
+			.abv(40)
+			.description("두강 맛 칵테일")
+			.flavor(flavor)
+			.imageUrl("https://naver.com")
+			.name("DOO")
+			.origin("두원이는 강하다.")
+			.build();
+
+		when(cocktailRepository.findById(anyLong())).thenReturn(Optional.of(blueHawaii));
+
+		CocktailDetailResponse cocktailDetailResponse = cocktailService.findCocktail(1L);
+
+		assertAll(
+			() -> assertThat(cocktailDetailResponse.getAbv()).isEqualTo(
+				blueHawaii.getAbv()),
+			() -> assertThat(cocktailDetailResponse.getDescription()).isEqualTo(
+				blueHawaii.getDescription()),
+			() -> assertThat(cocktailDetailResponse.isSweet()).isEqualTo(
+				blueHawaii.isSweet()),
+			() -> assertThat(cocktailDetailResponse.isSour()).isEqualTo(
+				blueHawaii.isSour()),
+			() -> assertThat(cocktailDetailResponse.isBitter()).isEqualTo(
+				blueHawaii.isBitter()),
+			() -> assertThat(cocktailDetailResponse.getImageUrl()).isEqualTo(
+				blueHawaii.getImageUrl()),
+			() -> assertThat(cocktailDetailResponse.getName()).isEqualTo(
+				blueHawaii.getName()),
+			() -> assertThat(cocktailDetailResponse.getOrigin()).isEqualTo(
+				blueHawaii.getOrigin()),
+			() -> assertThat(cocktailDetailResponse.getTags()).isEmpty(),
+			() -> assertThat(cocktailDetailResponse.getRecipe()).isEmpty()
+		);
 	}
 }
