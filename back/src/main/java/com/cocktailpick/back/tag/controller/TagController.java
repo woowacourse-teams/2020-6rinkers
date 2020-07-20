6e9a4b5a0031e4215c3514d06bfdd11ab1afc333
kuenhwi <kueniyam@naver.com>
2020-07-20 17:01:57 +0900

    feat: TagController, csv 추가 기능 구현

---
+++ back/src/main/java/com/cocktailpick/back/tag/controller/TagController.java
@@ -0,0 +1,27 @@
+package com.cocktailpick.back.tag.controller;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.cocktailpick.back.tag.service.TagService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/tags")
+public class TagController {
+	private final TagService tagService;
+
+	@PostMapping("/upload")
+	public ResponseEntity<Void> addTags(@RequestPart MultipartFile file) {
+		tagService.saveAll(file);
+
+		return ResponseEntity.created(URI.create("/api/tags")).build();
+	}
+}
---
+++ back/src/main/java/com/cocktailpick/back/tag/service/TagService.java
@@ -0,0 +1,15 @@
+package com.cocktailpick.back.tag.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class TagService {
+	public void saveAll(MultipartFile file) {
+
+	}
+}
+
