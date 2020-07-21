5b21f838e0fc141f88fa78f5232da41f3a6e71a4
ParkDooWon <pass345@naver.com>
2020-07-21 16:45:20 +0900

    test: CocktailRepositoryTest, 삭제 기능 test
    - orphanRemoval에 따라 RecipeItem 데이터가 삭제되는지 확인한다.

---
+++ back/src/test/java/com/cocktailpick/back/cocktail/domain/CocktailRepositoryTest.java
@@ -0,0 +1,60 @@
+package com.cocktailpick.back.cocktail.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.cocktailpick.back.recipe.domain.RecipeItem;
+import com.cocktailpick.back.recipe.domain.RecipeItemRepository;
+import com.cocktailpick.back.tag.domain.CocktailTag;
+import com.cocktailpick.back.tag.domain.Tag;
+import com.cocktailpick.back.tag.domain.TagRepository;
+
+@DataJpaTest
+public class CocktailRepositoryTest {
+	@Autowired
+	private CocktailRepository cocktailRepository;
+
+	@Autowired
+	private RecipeItemRepository recipeItemRepository;
+
+	@Autowired
+	private TagRepository tagRepository;
+
+	@Test
+	void delete() {
+		Flavor flavor = Flavor.builder()
+			.bitter(true)
+			.sour(true)
+			.sweet(false)
+			.build();
+
+		Cocktail blueHawaii = Cocktail.builder()
+			.abv(40)
+			.description("두강 맛 칵테일")
+			.flavor(flavor)
+			.imageUrl("https://naver.com")
+			.name("DOO")
+			.origin("두원이는 강하다.")
+			.build();
+
+		Tag tag = new Tag("두강맛");
+		tagRepository.save(tag);
+
+		RecipeItem recipeItem = RecipeItem.builder()
+			.ingredient("두강이")
+			.quantity("두ml")
+			.build();
+
+		recipeItem.setCocktail(blueHawaii);
+		CocktailTag.of(blueHawaii, tag);
+
+		cocktailRepository.saveAndFlush(blueHawaii);
+		assertThat(recipeItemRepository.findAll()).hasSize(1);
+
+		cocktailRepository.deleteById(blueHawaii.getId());
+		assertThat(recipeItemRepository.findAll()).isEmpty();
+	}
+}
