9beb76245d8522b51cfb553e9414505590acae4a
toneyparky <qkrtmddhks95@gmail.com>
2020-07-23 08:56:18 +0900

    fix: 관리자 페이지 header 오류 수정
    - 사용자 페이지의 header와 class명이 겹쳐서 발생
    - 새로운 class 명을 부여하여 해결

--- front/src/component/admin/AdminHeader.js
+++ front/src/component/admin/AdminHeader.js
@@ -4,7 +4,7 @@ import "../../css/common/header.css";
 const AdminHeader = () => {
   return (
     <div className="adminHeaderContainer">
-      <div className="title">칵테일 픽 관리자 페이지 - 칵테일</div>
+      <div className="adminTitle">칵테일 픽 관리자 페이지 - 칵테일</div>
     </div>
   );
 };
