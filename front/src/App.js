7ef806e2114edbedfda6bbca382ceed78e932832
toneyparky <qkrtmddhks95@gmail.com>
2020-07-22 13:33:50 +0900

    feat: 사용자와 관리자 페이지 각각 헤더 적용

--- front/src/App.js
+++ front/src/App.js
@@ -2,18 +2,20 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./index.css";
 import Header from "./component/common/Header";
-import Admin from "./component/admin/Main";
+import Admin from "./component/admin/Admin";
 import Home from "./component/home/Home";
+import AdminHeader from "./component/admin/AdminHeader";
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
       <Switch>
         <Route exact path="/">
+          <Header />
           <Home />
         </Route>
         <Route path="/admin">
+          <AdminHeader />
           <Admin />
         </Route>
       </Switch>
