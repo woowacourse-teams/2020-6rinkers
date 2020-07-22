56444b3f9f89a175da57478561aae8f9dabcd0aa
kuenhwi <kueniyam@naver.com>
2020-07-22 16:57:51 +0900

    refactor: CocktailCsvReader, 메서드 분리 및 상수화

--- back/src/main/java/com/cocktailpick/back/cocktail/service/CocktailCsvReader.java
+++ back/src/main/java/com/cocktailpick/back/cocktail/service/CocktailCsvReader.java
@@ -1,6 +1,5 @@
 package com.cocktailpick.back.cocktail.service;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,6 +15,8 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class CocktailCsvReader {
+	private static final String TRUE_VALUE = "1";
+
 	private static final int NAME_INDEX = 0;
 	private static final int ABV_INDEX = 1;
 	private static final int DESCRIPTION_INDEX = 2;
@@ -40,30 +41,31 @@ public class CocktailCsvReader {
 	public List<CocktailRequest> getCocktailRequests() {
 		List<String[]> lines = csvReader.readAll();
 
-		List<CocktailRequest> cocktailRequests = new ArrayList<>();
-		for (String[] line : lines) {
-			CocktailRequest cocktailRequest = CocktailRequest.builder()
-				.name(line[NAME_INDEX])
-				.abv(Double.parseDouble(line[ABV_INDEX]))
-				.description(line[DESCRIPTION_INDEX])
-				.origin(line[ORIGIN_INDEX])
-				.imageUrl(line[IMAGE_URL_INDEX])
-				.tag(splitAndTrim(line[TAG_INDEX]))
-				.sweet("1".equals(line[SWEET_INDEX]))
-				.sour("1".equals(line[SOUR_INDEX]))
-				.bitter("1".equals(line[BITTER_INDEX]))
-				.liquor(splitAndTrim(line[LIQUOR_INDEX]))
-				.liquorQuantity(splitAndTrim(line[LIQUOR_QUANTITY_INDEX]))
-				.special(splitAndTrim(line[SPECIAL_INDEX]))
-				.specialQuantity(splitAndTrim(line[SPECIAL_QUANTITY_INDEX]))
-				.build();
-			cocktailRequests.add(cocktailRequest);
-		}
-		return cocktailRequests;
+		return lines.stream()
+			.map(this::createCocktailRequest)
+			.collect(Collectors.toList());
+	}
+
+	private CocktailRequest createCocktailRequest(String[] line) {
+		return CocktailRequest.builder()
+			.name(line[NAME_INDEX])
+			.abv(Double.parseDouble(line[ABV_INDEX]))
+			.description(line[DESCRIPTION_INDEX])
+			.origin(line[ORIGIN_INDEX])
+			.imageUrl(line[IMAGE_URL_INDEX])
+			.tag(splitAndTrim(line[TAG_INDEX]))
+			.sweet(TRUE_VALUE.equals(line[SWEET_INDEX]))
+			.sour(TRUE_VALUE.equals(line[SOUR_INDEX]))
+			.bitter(TRUE_VALUE.equals(line[BITTER_INDEX]))
+			.liquor(splitAndTrim(line[LIQUOR_INDEX]))
+			.liquorQuantity(splitAndTrim(line[LIQUOR_QUANTITY_INDEX]))
+			.special(splitAndTrim(line[SPECIAL_INDEX]))
+			.specialQuantity(splitAndTrim(line[SPECIAL_QUANTITY_INDEX]))
+			.build();
 	}
 
 	private List<String> splitAndTrim(String input) {
-		return Arrays.stream(input.split(","))
+		return Arrays.stream(input.split(CsvReader.DELIMITER))
 			.map(String::trim)
 			.filter(StringUtils::isNotBlank)
 			.collect(Collectors.toList());
--- back/src/main/java/com/cocktailpick/back/common/csv/CsvReader.java
+++ back/src/main/java/com/cocktailpick/back/common/csv/CsvReader.java
@@ -3,5 +3,7 @@ package com.cocktailpick.back.common.csv;
 import java.util.List;
 
 public interface CsvReader {
+	String DELIMITER = ",";
+
 	List<String[]> readAll();
 }
