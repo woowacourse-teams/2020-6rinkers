98bbacf559c4606153f508205a70e958f7b92057
toneyparky <qkrtmddhks95@gmail.com>
2020-07-22 15:01:21 +0900

    refactor: service에 속성 추가
    - hover, divider 추가

--- front/src/component/common/Service.js
+++ front/src/component/common/Service.js
@@ -3,9 +3,11 @@ import React from "react";
 const Service = (props) => {
   return (
     <div className="service">
-      <div className="searchCocktail">칵테일 찾기</div>
-      <div className="searchBar">바 찾기</div>
-      <div className="recommendation">칵테일 추천 받기</div>
+      <div className="serviceItem searchCocktail">칵테일 찾기</div>
+      <div className="divider"></div>
+      <div className="serviceItem searchBar">바 찾기</div>
+      <div className="divider"></div>
+      <div className="serviceItem recommendation">칵테일 추천 받기</div>
     </div>
   );
 };
