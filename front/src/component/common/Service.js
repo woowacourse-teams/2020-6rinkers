cdeed8a85b19cfb3095ba92a0b669da50851352a
toneyparky <qkrtmddhks95@gmail.com>
2020-07-22 21:41:53 +0900

    feat: Service에 호버시 색 전환 기능 구현

--- front/src/component/common/Service.js
+++ front/src/component/common/Service.js
@@ -4,9 +4,7 @@ const Service = (props) => {
   return (
     <div className="service">
       <div className="serviceItem searchCocktail">칵테일 찾기</div>
-      <div className="divider"></div>
       <div className="serviceItem searchBar">바 찾기</div>
-      <div className="divider"></div>
       <div className="serviceItem recommendation">칵테일 추천 받기</div>
     </div>
   );
