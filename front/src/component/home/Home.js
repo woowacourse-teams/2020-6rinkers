868d4f4f935f4550b37087d3714614426f4aabec
toneyparky <qkrtmddhks95@gmail.com>
2020-07-22 15:52:17 +0900

    feat: Home 구현
    - cocktail 예시 이미지 추가
    - TodayCocktail, Recommendation 컴포넌트 추가

--- front/src/component/home/Home.js
+++ front/src/component/home/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import TodayCocktail from "./TodayCocktail";
+import "../../css/home/home.css";
+import Recommendation from "./Recommendation";
 
-function Home(props) {
-  return <div>HOME</div>;
-}
+const Home = (props) => {
+  return (
+    <div className="home">
+      <TodayCocktail />
+      <Recommendation />
+    </div>
+  );
+};
 
 export default Home;
---
+++ front/src/component/home/Recommendation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Recommendation = (props) => {
+  return (
+    <div className="homeRecommendation">
+      <div className="horizontalLine"></div>
+      <div className="content">오늘의 칵테일을 추천 받아 보아요</div>
+      <div className="horizontalLine"></div>
+    </div>
+  );
+};
+
+export default Recommendation;
