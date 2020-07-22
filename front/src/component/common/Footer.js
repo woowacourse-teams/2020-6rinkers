ee7baa2bf0f1d60904192db705d5ce7a7805d937
toneyparky <qkrtmddhks95@gmail.com>
2020-07-22 23:35:18 +0900

    style: footer에 border 추가 및 github 로고 변경

--- front/src/component/common/Footer.js
+++ front/src/component/common/Footer.js
@@ -4,15 +4,17 @@ import "../../css/common/footer.css";
 const Footer = (props) => {
   return (
     <div className="footerContainer">
-      <div className="link github">
-        <a href="https://github.com/woowacourse-teams/2020-6rinkers">
-          <img src="/image/github_logo.png" alt="github logo" />
-        </a>
-      </div>
-      <div className="link instagram">
-        <a href="https://www.instagram.com/6rinkers/">
-          <img src="/image/instagram_logo.png" alt="instagram logo" />
-        </a>
+      <div className="linkContainer">
+        <div className="link github">
+          <a href="https://github.com/woowacourse-teams/2020-6rinkers">
+            <img src="/image/github_logo.png" alt="github logo" />
+          </a>
+        </div>
+        <div className="link instagram">
+          <a href="https://www.instagram.com/6rinkers/">
+            <img src="/image/instagram_logo.png" alt="instagram logo" />
+          </a>
+        </div>
       </div>
     </div>
   );
