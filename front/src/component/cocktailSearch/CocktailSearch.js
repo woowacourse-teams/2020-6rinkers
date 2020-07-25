0e9571b3663e421f51b00cb4783852665df780f4
toneyparky <qkrtmddhks95@gmail.com>
2020-07-25 18:03:11 +0900

    feat: 검색창 html 추가 및 css 속성 추가

--- front/src/component/cocktailSearch/CocktailSearch.js
+++ front/src/component/cocktailSearch/CocktailSearch.js
@@ -1,12 +1,12 @@
 import React from "react";
 import SearchedCocktails from "./SearchedCocktails";
 import "../../css/cocktailSearch/cocktailSearch.css";
+import SearchContainer from "./SearchContainer";
 
 const CocktailSearch = () => {
   return (
     <div className="cocktailSearchContainer">
-      <div className="search">검색하기 창</div>
-      <div className="searchTags">태그 보여주기 창</div>
+      <SearchContainer />
       <SearchedCocktails />
     </div>
   );
---
+++ front/src/component/cocktailSearch/SearchContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SearchContainer = (props) => {
+  return (
+    <div className="searchContainer">
+      <div className="search">
+        <input type="text" placeholder="검색어를 입력하세요." />
+        <button type="submit">검색</button>
+      </div>
+      <div className="tagsContainer">태그 보여주기 창</div>
+    </div>
+  );
+}
+
+export default SearchContainer;
