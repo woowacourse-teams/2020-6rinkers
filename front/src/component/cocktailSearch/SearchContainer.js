c019fbd6e3278384e37c3160a0c08a108818510e
toneyparky <qkrtmddhks95@gmail.com>
2020-07-27 08:50:04 +0900

    refactor: 검색 버튼을 아이콘으로 수정

--- front/src/component/cocktailSearch/SearchContainer.js
+++ front/src/component/cocktailSearch/SearchContainer.js
@@ -5,7 +5,9 @@ const SearchContainer = (props) => {
     <div className="searchContainer">
       <div className="search">
         <input type="text" placeholder="검색어를 입력하세요." />
-        <button type="submit">검색</button>
+        <div className="searchButtonContainer">
+          <img className="searchButton" src="/image/search.svg" alt="search" />
+        </div>
       </div>
       <div className="tagsContainer">태그 보여주기 창</div>
     </div>
