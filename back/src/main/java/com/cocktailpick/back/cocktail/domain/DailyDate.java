61763e34af67cf0ab6b33050d37cf0881461b4de
kuenhwi <kueniyam@naver.com>
2020-07-27 15:38:40 +0900

    feat: 입력 받은 Date에서 그 날짜의 Date를 구하는 기능 구현

---
+++ back/src/main/java/com/cocktailpick/back/cocktail/domain/DailyDate.java
@@ -0,0 +1,33 @@
+package com.cocktailpick.back.cocktail.domain;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class DailyDate {
+	private final Date date;
+
+	public static DailyDate of(Date date) {
+		try {
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+			String format = simpleDateFormat.format(date);
+
+			return new DailyDate(simpleDateFormat.parse(format));
+		} catch (ParseException e) {
+			throw new RuntimeException();
+		}
+	}
+
+	public Date getDate() {
+		return date;
+	}
+}
+
---
+++ back/src/test/java/com/cocktailpick/back/cocktail/domain/DailyDateTest.java
@@ -0,0 +1,29 @@
+package com.cocktailpick.back.cocktail.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class DailyDateTest {
+	@DisplayName("입력 받은 Date 값에서 일년월을 제외한 값을 0으로 만든다.")
+	@ParameterizedTest
+	@CsvSource(value = {"2020,7,27,23,59", "2020,7,27,0,1"})
+	void of_SameDay(int year, int month, int date, int hour, int minute) {
+		Date sixOClock = createDate(year, month, date, hour, minute);
+
+		assertThat(DailyDate.of(sixOClock).getDate())
+			.isEqualTo(createDate(2020, 7, 27, 0, 0));
+	}
+
+	private Date createDate(int year, int month, int date, int hour, int minute) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.clear();
+		calendar.set(year, month - 1, date, hour, minute);
+		return calendar.getTime();
+	}
+}
