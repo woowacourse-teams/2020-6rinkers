cc1e0d89b12188b22bff7c51c8f969c70da982f7
minuyim <minu_yim@naver.com>
2020-07-28 16:08:47 +0900

    refactor: BusinessException 불필요한 생성자 삭제 및 @Getter 추가

--- back/src/main/java/com/cocktailpick/back/common/exceptions/BusinessException.java
+++ back/src/main/java/com/cocktailpick/back/common/exceptions/BusinessException.java
@@ -1,19 +1,13 @@
 package com.cocktailpick.back.common.exceptions;
 
+import lombok.Getter;
+
+@Getter
 public class BusinessException extends RuntimeException {
 	private final ErrorCode errorCode;
-
-	public BusinessException(String message, ErrorCode errorCode) {
-		super(message);
-		this.errorCode = errorCode;
-	}
 
 	public BusinessException(ErrorCode errorCode) {
 		super(errorCode.getMessage());
 		this.errorCode = errorCode;
-	}
-
-	public ErrorCode getErrorCode() {
-		return errorCode;
 	}
 }
