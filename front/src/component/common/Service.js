07cc2b0d8569be0080cfa7f09ddd4f6862539a45
hwangbo <taekwondo714@naver.com>
2020-07-30 17:31:11 +0900

    style: 모듈에서 사용하는 클래스와 중복되는 className 수정

--- front/src/component/common/Service.js
+++ front/src/component/common/Service.js
@@ -7,12 +7,12 @@ const Service = () => {
       <NavLink
         to="/cocktails/search"
         className="serviceItem"
-        activeClassName="active"
+        activeClassName="nav-active"
       >
         칵테일 찾기
       </NavLink>
       <div className="serviceItem searchBar">바 찾기</div>
-      <NavLink to="/recommend" className="serviceItem" activeClassName="active">
+      <NavLink to="/recommend" className="serviceItem" activeClassName="nav-active">
         칵테일 추천 받기
       </NavLink>
     </div>
