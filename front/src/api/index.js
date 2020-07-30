ac3fa347d8cd0badd3aeb62abdc148060d442624
ParkDooWon <pass345@naver.com>
2020-07-30 21:44:46 +0900

    feat: 배포 환경을 구축한다.

--- front/src/api/index.js
+++ front/src/api/index.js
@@ -1,20 +1,23 @@
 import axios from "axios";
 
-const client = axios.create({});
+const client = axios.create({
+    baseURL: 'http://13.209.150.130:8080'
+});
 
 export const fetchAllCocktails = () => client.get("/api/cocktails");
 export const fetchCocktail = (id) => client.get(`/api/cocktails/${id}`);
 export const fetchTodayCocktail = () => client.get("/api/cocktails/today");
-export const createCocktail = (data) => client.post("/api/cocktails", data);
+export const createCocktail = (data) =>
+    client.post("/api/cocktails", data);
 export const createCocktailsByCsv = (file) =>
-  client.post("/api/cocktails/upload/csv", file, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
+    client.post("/api/cocktails/upload/csv", file, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    });
 export const createTagsByCsv = (file) =>
-  client.post("/api/tags/upload/csv", file, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
+    client.post("/api/tags/upload/csv", file, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    });
