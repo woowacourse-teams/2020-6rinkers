fb8e13a6cd73e54bc352a386471b05338bab0e21
toneyparky <qkrtmddhks95@gmail.com>
2020-07-31 15:36:13 +0900

    refactor: 네브바 선택시 색상 변경

--- front/src/component/common/ServicesSlider.js
+++ front/src/component/common/ServicesSlider.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const ServiceSlider = ({ slider }) => {
   const isActive = slider ? "serviceSlider active" : "serviceSlider";
   return (
     <div className={isActive}>
-      <div className="serviceSliderItem">칵테일 찾기</div>
-      <div className="serviceSliderItem">바 찾기</div>
-      <div className="serviceSliderItem">칵테일 추천 받기</div>
+      <NavLink
+        to="/cocktails/search"
+        className="serviceSliderItem"
+        activeClassName="navitem-active"
+      >
+        칵테일 찾기
+      </NavLink>
+      <div className="serviceSliderItem searchBar">바 찾기</div>
+      <NavLink
+        to="/recommend"
+        className="serviceSliderItem"
+        activeClassName="navitem-active"
+      >
+        칵테일 추천 받기
+      </NavLink>
     </div>
   );
 };
