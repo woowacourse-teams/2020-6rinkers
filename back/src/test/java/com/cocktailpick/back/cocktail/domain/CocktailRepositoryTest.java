536e1ea22065d56afbedafc1c1530b158561c33d
toneyparky <qkrtmddhks95@gmail.com>
2020-07-31 20:51:08 +0900

    feat: CocktailRepository, 원하는 수 만큼 칵테일 조회 기능 구현
    - findByIdGreaterThan 생성

--- back/src/test/java/com/cocktailpick/back/cocktail/domain/CocktailRepositoryTest.java
+++ back/src/test/java/com/cocktailpick/back/cocktail/domain/CocktailRepositoryTest.java
@@ -2,10 +2,15 @@ package com.cocktailpick.back.cocktail.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import com.cocktailpick.back.recipe.domain.RecipeItem;
 import com.cocktailpick.back.recipe.domain.RecipeItemRepository;
@@ -28,11 +33,15 @@ public class CocktailRepositoryTest {
 
 	private Cocktail blueHawaii;
 
+	private Flavor flavor;
+
 	@BeforeEach
 	void setUp() {
 		tag = new Tag("두강맛");
 
-		Flavor flavor = Flavor.builder()
+		tagRepository.save(tag);
+
+		flavor = Flavor.builder()
 			.bitter(true)
 			.sour(true)
 			.sweet(false)
@@ -48,9 +57,59 @@ public class CocktailRepositoryTest {
 			.build();
 	}
 
+	@DisplayName("칵테일을 특정 크기만큼 조회한다.")
+	@Test
+	void findByIdGreaterThan() {
+		RecipeItem martiniRecipe = RecipeItem.builder()
+			.ingredient("gin")
+			.quantity("30ml")
+			.build();
+
+		RecipeItem ginTonicRecipe = RecipeItem.builder()
+			.ingredient("gin")
+			.quantity("45ml")
+			.build();
+
+		Cocktail martini = Cocktail.builder()
+			.abv(40)
+			.description("movie 007")
+			.flavor(flavor)
+			.imageUrl("https://naver.com")
+			.name("martini")
+			.origin("stir don't shake")
+			.build();
+
+		Cocktail ginTonic = Cocktail.builder()
+			.abv(25)
+			.description("classic cocktail")
+			.flavor(flavor)
+			.imageUrl("https://naver.com")
+			.name("gin tonic")
+			.origin("I don't know")
+			.build();
+
+		martiniRecipe.setCocktail(martini);
+		CocktailTag.associate(martini, tag);
+		ginTonicRecipe.setCocktail(ginTonic);
+		CocktailTag.associate(ginTonic, tag);
+
+		cocktailRepository.saveAll(Arrays.asList(blueHawaii, martini, ginTonic));
+
+		Pageable pageRequest = PageRequest.of(0, 2);
+		assertThat(cocktailRepository.findByIdGreaterThan(1, pageRequest).getContent()).hasSize(
+			pageRequest.getPageSize());
+	}
+
+	@DisplayName("저장된 칵테일이 없을시 페이지로 조회할 경우 빈 문자열을 담은 Pageable 객체를 반환한다.")
+	@Test
+	void findByIdGreaterThan_whenNoData_returnEmptyList() {
+		Pageable pageRequest = PageRequest.of(0, 2);
+		assertThat(cocktailRepository.findByIdGreaterThan(1, pageRequest).getContent()).hasSize(0);
+	}
+
+	@DisplayName("칵테일이 삭제될 경우 recipeItem도 삭제한다.")
 	@Test
 	void delete() {
-		tagRepository.save(tag);
 
 		RecipeItem recipeItem = RecipeItem.builder()
 			.ingredient("두강이")
