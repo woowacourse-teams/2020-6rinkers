2c71a47e55157db09364541712cb3a6e9bfabca3
toneyparky <qkrtmddhks95@gmail.com>
2020-08-01 14:54:24 +0900

    feat: 프론트에 반응형 적용

--- front/src/api/index.js
+++ front/src/api/index.js
@@ -5,20 +5,22 @@ const client = axios.create({
 });
 
 export const fetchAllCocktails = () => client.get("/api/cocktails");
+export const fetchPagedCocktails = (id, size) =>
+  client.get("/api/cocktails/pages", { params: { id, size } });
 export const fetchCocktail = (id) => client.get(`/api/cocktails/${id}`);
 export const fetchTodayCocktail = () => client.get("/api/cocktails/today");
 export const createCocktail = (data) => client.post("/api/cocktails", data);
 export const createRecommend = (recommend) =>
   client.post(`/api/cocktails/recommend`, recommend);
 export const createCocktailsByCsv = (file) =>
-    client.post("/api/cocktails/upload/csv", file, {
-        headers: {
-            "Content-Type": "multipart/form-data",
-        },
-    });
+  client.post("/api/cocktails/upload/csv", file, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 export const createTagsByCsv = (file) =>
-    client.post("/api/tags/upload/csv", file, {
-        headers: {
-            "Content-Type": "multipart/form-data",
-        },
-    });
+  client.post("/api/tags/upload/csv", file, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
--- front/src/component/cocktailSearch/CocktailSearch.js
+++ front/src/component/cocktailSearch/CocktailSearch.js
@@ -1,26 +1,65 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import SearchedCocktails from "./SearchedCocktails";
 import "../../css/cocktailSearch/cocktailSearch.css";
 import SearchContainer from "./SearchContainer";
-import { fetchAllCocktails } from "../../api";
+import { fetchPagedCocktails } from "../../api";
 
 const CocktailSearch = () => {
+  const [loading, setLoading] = useState(false);
   const [cocktails, setCocktails] = useState([
     {
-      // 테스트 용. 추후 제거 필요
+      id: 0,
       name: "블루하와이",
       imageUrl: "/image/blue-hawai.png",
     },
   ]);
 
-  const onLoadCocktails = async () => {
-    const response = await fetchAllCocktails();
-    const content = response.data;
-    setCocktails(content);
+  const [lastCocktailId, setLastCocktailId] = useState(0);
+
+  const toggleLoading = async () => {
+    await setLoading(!loading);
   };
 
+  const onInitialLoadCocktails = async () => {
+    const response = await fetchPagedCocktails(lastCocktailId, 15);
+    const content = response.data;
+
+    setCocktails(content);
+    setLastCocktailId(content[content.length - 1].id);
+  };
+
+  const onLoadCocktails = async (size) => {
+    if (loading) {
+      return;
+    }
+    const response = await fetchPagedCocktails(lastCocktailId, size);
+    const content = response.data;
+    setCocktails(cocktails.concat(content));
+    setLastCocktailId(cocktails[cocktails.length - 1].id);
+  };
+
+  const infiniteScroll = useCallback(async () => {
+    const size = window.innerWidth > 700 ? 18 : 6;
+    const threshold = window.innerWidth > 700 ? 1600 : 1300;
+
+    if (
+      document.documentElement.scrollTop +
+        document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight - threshold
+    ) {
+      await toggleLoading();
+      setLastCocktailId(cocktails[cocktails.length - 1].id);
+      await onLoadCocktails(size);
+    }
+  }, [cocktails, lastCocktailId]);
+
   useEffect(() => {
-    onLoadCocktails();
+    window.addEventListener("scroll", infiniteScroll, true);
+    return () => window.removeEventListener("scroll", infiniteScroll, true);
+  }, [infiniteScroll]);
+
+  useEffect(() => {
+    onInitialLoadCocktails();
   }, []);
 
   return (
