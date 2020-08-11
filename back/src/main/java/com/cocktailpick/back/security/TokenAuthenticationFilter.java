59d710eda8dc90946ac9d479d99ec0c0c42f5e8f
hwangbo <taekwonod714@naver.com>
2020-08-11 16:09:22 +0900

    feat: 인증 관련 클래스 구현

---
+++ back/src/main/java/com/cocktailpick/back/security/TokenAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.cocktailpick.back.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+	private static final Logger logger = LoggerFactory.getLogger(TokenAuthenticationFilter.class);
+	@Autowired
+	private TokenProvider tokenProvider;
+	@Autowired
+	private CustomUserDetailsService customUserDetailsService;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws
+		ServletException,
+		IOException {
+		try {
+			String jwt = getJwtFromRequest(request);
+
+			if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+				Long userId = tokenProvider.getUserIdFromToken(jwt);
+
+				UserDetails userDetails = customUserDetailsService.loadUserById(userId);
+				UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+					userDetails, null, userDetails.getAuthorities());
+				authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+			}
+		} catch (Exception ex) {
+			logger.error("Could not set user authentication in security context", ex);
+		}
+
+		filterChain.doFilter(request, response);
+	}
+
+	private String getJwtFromRequest(HttpServletRequest request) {
+		String bearerToken = request.getHeader("Authorization");
+		if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+			return bearerToken.substring(7);
+		}
+		return null;
+	}
+}
