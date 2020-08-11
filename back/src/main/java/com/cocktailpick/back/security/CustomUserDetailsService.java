ab1632c54eeff644342065ab277ada47c205ddbb
hwangbo <taekwonod714@naver.com>
2020-08-11 15:59:41 +0900

    feat: 이메일 기반 인증 컨트롤러 및 서비스 구현

---
+++ back/src/main/java/com/cocktailpick/back/security/CustomUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.cocktailpick.back.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cocktailpick.back.common.exceptions.ResourceNotFoundException;
+import com.cocktailpick.back.user.domain.User;
+import com.cocktailpick.back.user.domain.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String email)
+		throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(email)
+			.orElseThrow(() ->
+				new UsernameNotFoundException("User not found with email : " + email)
+			);
+
+		return UserPrincipal.create(user);
+	}
+
+	@Transactional
+	public UserDetails loadUserById(Long id) {
+		User user = userRepository.findById(id).orElseThrow(
+			() -> new ResourceNotFoundException("User", "id", id)
+		);
+
+		return UserPrincipal.create(user);
+	}
+}
