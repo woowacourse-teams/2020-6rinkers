c3e69d7734ad61460d19316bc8c0890ad9dfdefe
ParkDooWon <pass345@naver.com>
2020-08-11 19:59:40 +0900

    feat: Tag 관련 API 문서화

--- back/src/test/java/com/cocktailpick/back/tag/controller/TagControllerTest.java
+++ back/src/test/java/com/cocktailpick/back/tag/controller/TagControllerTest.java
@@ -9,37 +9,24 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
+import com.cocktailpick.back.common.documentation.Documentation;
+import com.cocktailpick.back.tag.docs.TagDocumentation;
 import com.cocktailpick.back.tag.dto.TagRequest;
 import com.cocktailpick.back.tag.dto.TagResponse;
 import com.cocktailpick.back.tag.service.TagService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(controllers = {TagController.class})
-class TagControllerTest {
+class TagControllerTest extends Documentation {
 	@MockBean
 	private TagService tagService;
-
-	private MockMvc mockMvc;
-
-	@BeforeEach
-	public void setUp(WebApplicationContext webApplicationContext) {
-		this.mockMvc = MockMvcBuilders
-			.webAppContextSetup(webApplicationContext)
-			.addFilters(new CharacterEncodingFilter("UTF-8", true))
-			.build();
-	}
 
 	@DisplayName("태그 csv 파일을 저장한다.")
 	@Test
@@ -52,7 +39,8 @@ class TagControllerTest {
 			.contentType(MediaType.MULTIPART_FORM_DATA))
 			.andExpect(status().isCreated())
 			.andExpect(header().string("Location", "/api/tags"))
-			.andDo(print());
+			.andDo(print())
+			.andDo(TagDocumentation.upload());
 	}
 
 	@DisplayName("모든 태그를 조회한다.")
@@ -66,7 +54,8 @@ class TagControllerTest {
 		mockMvc.perform(get("/api/tags")
 			.accept(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
-			.andDo(print());
+			.andDo(print())
+			.andDo(TagDocumentation.findTags());
 	}
 
 	@DisplayName("태그를 생성한다.")
@@ -80,6 +69,7 @@ class TagControllerTest {
 			.content(new ObjectMapper().writeValueAsString(tagRequest)))
 			.andExpect(status().isCreated())
 			.andExpect(header().string("Location", "/api/tags/1"))
-			.andDo(print());
+			.andDo(print())
+			.andDo(TagDocumentation.create());
 	}
 }
---
+++ back/src/test/java/com/cocktailpick/back/tag/docs/TagDocumentation.java
@@ -0,0 +1,38 @@
+package com.cocktailpick.back.tag.docs;
+
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.payload.JsonFieldType;
+
+public class TagDocumentation {
+	public static RestDocumentationResultHandler upload() {
+		return document("tags/upload",
+			requestParts(
+				partWithName("file").description("태그 csv 파일")
+			)
+		);
+	}
+
+	public static RestDocumentationResultHandler findTags() {
+		return document("tags/findAll",
+			responseFields(
+				fieldWithPath("[].name").type(JsonFieldType.STRING).description("태그 이름")
+			)
+		);
+	}
+
+	public static RestDocumentationResultHandler create() {
+		return document("tags/create",
+			requestFields(
+				fieldWithPath("name").type(JsonFieldType.STRING).description("생성할 태그 이름")
+			),
+			responseHeaders(
+				headerWithName("Location").description("생성한 태그 ID")
+			)
+		);
+	}
+}
