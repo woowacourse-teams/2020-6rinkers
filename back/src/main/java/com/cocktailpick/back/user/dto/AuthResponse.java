1ab84902acb7e0a53e6cd4104e09fded5651c6d3
hwangbo <taekwonod714@naver.com>
2020-08-11 15:59:41 +0900

    feat: 이메일 기반 인증 컨트롤러 및 서비스 구현

---
+++ back/src/main/java/com/cocktailpick/back/user/dto/AuthResponse.java
@@ -0,0 +1,26 @@
+package com.cocktailpick.back.user.dto;
+
+public class AuthResponse {
+	private String accessToken;
+	private String tokenType = "Bearer";
+
+	public AuthResponse(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public String getTokenType() {
+		return tokenType;
+	}
+
+	public void setTokenType(String tokenType) {
+		this.tokenType = tokenType;
+	}
+}
