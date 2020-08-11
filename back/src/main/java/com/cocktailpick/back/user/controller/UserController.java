888fe43f9db9678557bb43b0b24d6c439e39a7dc
hwangbo <taekwonod714@naver.com>
2020-08-11 16:10:42 +0900

    feat: UserController 구현

---
+++ back/src/main/java/com/cocktailpick/back/user/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.cocktailpick.back.user.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cocktailpick.back.common.exceptions.ResourceNotFoundException;
+import com.cocktailpick.back.security.CurrentUser;
+import com.cocktailpick.back.security.UserPrincipal;
+import com.cocktailpick.back.user.domain.User;
+import com.cocktailpick.back.user.domain.UserRepository;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@GetMapping("/user/me")
+	@PreAuthorize("hasRole('USER')")
+	public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+		return userRepository.findById(userPrincipal.getId())
+			.orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
+	}
+}
