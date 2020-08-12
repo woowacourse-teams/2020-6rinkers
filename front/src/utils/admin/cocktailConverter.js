004a7d60eb228222146b929957e51e69648c5a91
toneyparky <qkrtmddhks95@gmail.com>
2020-08-13 08:11:34 +0900

    feat: 칵테일 정보를 input 정보로 바꿔주는 유틸 메서드 추가
    - convertCocktailToInputData

--- front/src/utils/admin/cocktailConverter.js
+++ front/src/utils/admin/cocktailConverter.js
@@ -21,4 +21,26 @@ export const convertDataToCocktailRequest = (data) => {
   return cocktailRequest;
 };
 
-export default dataToCocktailRequest;
+export const convertCocktailToInputData = (cocktail) => {
+  const inputData = {
+    id: cocktail.id,
+    name: cocktail.name,
+    abv: parseFloat(cocktail.abv),
+    description: cocktail.description,
+    origin: cocktail.origin,
+    imageUrl: cocktail.imageUrl,
+    tag: tagsToInputTags(cocktail.tags),
+    sweet: cocktail.sweet ? 1 : 0,
+    sour: cocktail.sour ? 1 : 0,
+    bitter: cocktail.bitter ? 1 : 0,
+    liquor: cocktail.recipe.map(e => e.ingredient).join(","),
+    liquorQuantity: cocktail.recipe.map(e => e.quantity).join(","),
+    special: "",
+    specialQuantity: "",
+  };
+  return inputData;
+};
+
+const tagsToInputTags = (tags) => {
+  return tags.map(e => e.name).join(",");
+}
