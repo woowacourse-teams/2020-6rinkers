94931041bcbb6d2c4c13dde322d14d907c964337
ParkDooWon <pass345@naver.com>
2020-08-13 14:29:36 +0900

    feat: 구글 애널리틱스를 위한 설정을 추가한다.

--- front/src/index.js
+++ front/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import ReactGA from "react-ga";
+
+ReactGA.initialize("UA-175294078-1");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <BrowserRouter>
