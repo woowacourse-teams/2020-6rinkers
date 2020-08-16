a89b0de66843ee2b95e5fbea3b441083cff5ed6b
ParkDooWon <pass345@naver.com>
2020-08-16 21:50:45 +0900

    refactor: tagType에 대한 문서화 추가

--- back/src/test/java/com/cocktailpick/back/tag/docs/TagDocumentation.java
+++ back/src/test/java/com/cocktailpick/back/tag/docs/TagDocumentation.java
@@ -20,7 +20,8 @@ public class TagDocumentation {
 	public static RestDocumentationResultHandler findTags() {
 		return document("tags/findAll",
 			responseFields(
-				fieldWithPath("[].name").type(JsonFieldType.STRING).description("태그 이름")
+				fieldWithPath("[].name").type(JsonFieldType.STRING).description("태그 이름"),
+				fieldWithPath("[].tagType").type(JsonFieldType.STRING).description("태그 타입")
 			)
 		);
 	}
@@ -28,7 +29,8 @@ public class TagDocumentation {
 	public static RestDocumentationResultHandler create() {
 		return document("tags/create",
 			requestFields(
-				fieldWithPath("name").type(JsonFieldType.STRING).description("생성할 태그 이름")
+				fieldWithPath("name").type(JsonFieldType.STRING).description("생성할 태그 이름"),
+				fieldWithPath("tagType").type(JsonFieldType.STRING).description("생성할 태그의 타입")
 			),
 			responseHeaders(
 				headerWithName("Location").description("생성한 태그 ID")
