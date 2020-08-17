c6e0f3c25348174f53cdd6c71ad6b0393687ded1
toneyparky <qkrtmddhks95@gmail.com>
2020-08-17 19:50:45 +0900

    feat: Ingredient 질문 구현

--- front/src/component/recommend/Ingredient.js
+++ front/src/component/recommend/Ingredient.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import Tag from "./Tag";
+import NextStage from "./NextStage";
 
-const Ingredient = (props) => {
-    return (
-        <div>4</div>
-    );
-}
+const Ingredient = ({ addAnswer }) => {
+  const [ingredients, setIngredients] = useState([]);
+  const [answer, setAnswer] = useState({});
+
+  useEffect(() => {
+    // api
+    const updateIngredients = async () => {
+      await setIngredients(["라임", "레몬", "맥주"]);
+    };
+    updateIngredients();
+  }, []);
+
+  useEffect(() => {
+    if (ingredients.length === 0) {
+      return;
+    }
+    const updateAnswer = async () => {
+      await setAnswer(
+        ingredients.reduce((o, key) => ({ ...o, [key]: "SOSO" }), {})
+      );
+    };
+    updateAnswer();
+  }, [ingredients]);
+
+  const onChangeAnswer = (name, userAnswer) => {
+    setAnswer({
+      ...answer,
+      [name]: userAnswer,
+    });
+  };
+
+  return (
+    <div>
+      {ingredients &&
+        ingredients.map((concept, index) => {
+          return (
+            <Tag
+              name={concept}
+              key={`key-${index}`}
+              answer={answer}
+              onChangeAnswer={onChangeAnswer}
+            />
+          );
+        })}
+      <NextStage
+        type="preferenceAnswers"
+        answer={answer}
+        addAnswer={addAnswer}
+        saying="다음 질문으로"
+      />
+    </div>
+  );
+};
 
 export default Ingredient;
