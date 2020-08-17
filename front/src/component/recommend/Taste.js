837db75b9775f634e8f3bcfb681ecd40586c083d
toneyparky <qkrtmddhks95@gmail.com>
2020-08-17 20:03:03 +0900

    feat: Taste 질문 구현

--- front/src/component/recommend/Taste.js
+++ front/src/component/recommend/Taste.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import Tag from "./Tag";
+import NextStage from "./NextStage";
 
-const Taste = (props) => {
-    return (
-        <div>5</div>
-    );
-}
+const Taste = ({ addAnswer }) => {
+  const tastes = ["sweetAnswer", "sourAnswer", "bitterAnswer"];
+  const [answer, setAnswer] = useState({
+    sweetAnswer: "",
+    sourAnswer: "",
+    bitterAnswer: "",
+  });
+
+  const onChangeAnswer = (name, userAnswer) => {
+    setAnswer({
+      ...answer,
+      [name]: userAnswer,
+    });
+  };
+
+  return (
+    <div>
+      {tastes.map((concept, index) => {
+        return (
+          <Tag
+            name={concept}
+            key={`key-${index}`}
+            answer={answer}
+            onChangeAnswer={onChangeAnswer}
+          />
+        );
+      })}
+      <NextStage
+        type="flavorAnswer"
+        answer={answer}
+        addAnswer={addAnswer}
+        saying="마지막 질문으로"
+      />
+    </div>
+  );
+};
 
 export default Taste;
