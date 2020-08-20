b848397ba3de389c31b486df694982b78cbf364b
toneyparky <qkrtmddhks95@gmail.com>
2020-08-20 10:28:47 +0900

    refactor: CircularBox의 배경색을 지정할 수 있도록 수정
    - padding 수정

--- front/src/component/common/CircularBox.js
+++ front/src/component/common/CircularBox.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "../../css/common/circularBox.css";
 
-const CircularBox = ({ text }) => {
+const CircularBox = ({ text, color }) => {
   return (
     <>
-      <div className="circular-box">{text}</div>
+      <div className="circular-box" style={{backgroundColor: color}}>{text}</div>
     </>
   );
 };
