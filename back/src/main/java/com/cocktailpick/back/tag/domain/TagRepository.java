3a45ca071e9ff65c56a1b50d8b85adc37a6eda3c
minuyim <minu_yim@naver.com>
2020-08-20 13:02:36 +0900

    feat: TagRepository에서 TagType을 조건으로 Tag를 찾는 기능 추가

--- back/src/main/java/com/cocktailpick/back/tag/domain/TagRepository.java
+++ back/src/main/java/com/cocktailpick/back/tag/domain/TagRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
 	List<Tag> findByNameIn(List<String> names);
+
+	List<Tag> findByTagType(TagType tagType);
 }
