5b3f37ff7257f09e43a528710db4f3b442e607fc
hwangbo <taekwonod714@naver.com>
2020-08-21 14:57:25 +0900

    fix: rebase 누락 부분 수정

--- front/src/component/admin/tag/TagAdmin.js
+++ front/src/component/admin/tag/TagAdmin.js
@@ -21,7 +21,7 @@ const TagContainer = styled.div`
   overflow: auto !important;
 `;
 
-const TagAdmin = () => {
+const TagAdmin = ({ role }) => {
   const [tagName, setTagName] = useState("");
   const [tagType, setTagType] = useState("");
 
--- front/src/constants/index.js
+++ front/src/constants/index.js
@@ -1,6 +1,7 @@
 export const DOWN = 40;
 export const UP = 38;
 export const ENTER = 13;
+export const ESC = 27;
 
 export const USER_PROTOTYPE = {
   id: 0,
