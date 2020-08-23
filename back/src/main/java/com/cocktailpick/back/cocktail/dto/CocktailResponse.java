dbd2eb67d639e8fa41db443f8cbd6552b83b9d68
toneyparky <qkrtmddhks95@gmail.com>
2020-08-23 23:36:39 +0900

    feat: 즐겨찾기 여부를 확인하여 칵테일 응답 DTO 필드에 추가

--- back/src/main/java/com/cocktailpick/back/cocktail/dto/CocktailResponse.java
+++ back/src/main/java/com/cocktailpick/back/cocktail/dto/CocktailResponse.java
@@ -1,9 +1,10 @@
 package com.cocktailpick.back.cocktail.dto;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.cocktailpick.back.cocktail.domain.Cocktail;
+import com.cocktailpick.back.favorite.domain.Favorites;
 import com.cocktailpick.back.tag.dto.TagResponse;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -24,14 +25,24 @@ public class CocktailResponse {
 
 	private List<TagResponse> tags;
 
-	public static CocktailResponse of(Cocktail cocktail) {
+	private boolean isFavorite;
+
+	public static CocktailResponse of(Cocktail cocktail, boolean isFavorite) {
 		return new CocktailResponse(cocktail.getId(), cocktail.getName(), cocktail.getImageUrl(),
-			TagResponse.listOf(cocktail.getTags()));
+			TagResponse.listOf(cocktail.getTags()), isFavorite);
 	}
 
-	public static List<CocktailResponse> listOf(List<Cocktail> cocktails) {
-		return cocktails.stream()
-			.map(CocktailResponse::of)
-			.collect(Collectors.toList());
+	public static List<CocktailResponse> listOf(List<Cocktail> cocktails, Favorites favorites) {
+		List<CocktailResponse> cocktailResponses = new ArrayList<>();
+
+		for (Cocktail cocktail : cocktails) {
+			if (favorites.isContainCocktail(cocktail)) {
+				cocktailResponses.add(CocktailResponse.of(cocktail, true));
+			} else {
+				cocktailResponses.add(CocktailResponse.of(cocktail, false));
+			}
+		}
+
+		return cocktailResponses;
 	}
 }
