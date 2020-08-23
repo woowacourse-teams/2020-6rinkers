3ce6c5a8fb8afdfa3760f21390770a049d5c63b6
toneyparky <qkrtmddhks95@gmail.com>
2020-08-23 17:26:45 +0900

    feat: addFavorite 서비스 구현

--- back/src/main/java/com/cocktailpick/back/user/service/UserService.java
+++ back/src/main/java/com/cocktailpick/back/user/service/UserService.java
@@ -1,8 +1,13 @@
 package com.cocktailpick.back.user.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.cocktailpick.back.cocktail.domain.Cocktail;
 import com.cocktailpick.back.cocktail.domain.CocktailRepository;
+import com.cocktailpick.back.common.exceptions.EntityNotFoundException;
+import com.cocktailpick.back.common.exceptions.ErrorCode;
+import com.cocktailpick.back.favorite.domain.Favorite;
 import com.cocktailpick.back.favorite.dto.FavoriteRequest;
 import com.cocktailpick.back.favorite.service.FavoriteRepository;
 import com.cocktailpick.back.user.domain.User;
@@ -17,7 +22,15 @@ public class UserService {
 	private final FavoriteRepository favoriteRepository;
 	private final CocktailRepository cocktailRepository;
 
+	@Transactional
 	public Long addFavorite(User user, FavoriteRequest favoriteRequest) {
-		return 1L;
+		Favorite favorite = new Favorite();
+		Cocktail cocktail = cocktailRepository.findById(favoriteRequest.getCocktailId())
+			.orElseThrow(() -> new EntityNotFoundException(ErrorCode.COCKTAIL_NOT_FOUND));
+
+		favorite.setCocktail(cocktail);
+		favorite.setUser(user);
+
+		return favoriteRepository.save(favorite).getId();
 	}
 }
---
+++ back/src/test/java/com/cocktailpick/back/user/service/UserServiceTest.java
@@ -0,0 +1,63 @@
+package com.cocktailpick.back.user.service;
+
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.cocktailpick.back.cocktail.domain.Cocktail;
+import com.cocktailpick.back.cocktail.domain.CocktailRepository;
+import com.cocktailpick.back.favorite.domain.Favorite;
+import com.cocktailpick.back.favorite.dto.FavoriteRequest;
+import com.cocktailpick.back.favorite.service.FavoriteRepository;
+import com.cocktailpick.back.user.domain.User;
+import com.cocktailpick.back.user.domain.UserRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+	private UserService userService;
+
+	@Mock
+	private UserRepository userRepository;
+
+	@Mock
+	private FavoriteRepository favoriteRepository;
+
+	@Mock
+	private CocktailRepository cocktailRepository;
+
+	private User user;
+
+	private Cocktail cocktail;
+
+	private Favorite favorite;
+
+	@BeforeEach
+	void setUp() {
+		userService = new UserService(userRepository, favoriteRepository, cocktailRepository);
+		user = User.builder()
+			.name("toney")
+			.build();
+		cocktail = Cocktail.builder()
+			.id(1L)
+			.name("toney")
+			.build();
+		favorite = new Favorite(1L, user, cocktail);
+	}
+
+	@DisplayName("즐겨찾기를 추가한다.")
+	@Test
+	void addFavoriteTest() {
+		when(cocktailRepository.findById(anyLong())).thenReturn(Optional.of(cocktail));
+		when(favoriteRepository.save(any())).thenReturn(favorite);
+		userService.addFavorite(user, new FavoriteRequest(1L));
+
+		verify(favoriteRepository).save(any());
+	}
+}
