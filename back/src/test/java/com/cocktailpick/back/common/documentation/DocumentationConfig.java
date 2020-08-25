93624c5cff4b205e07a14795c2e279283baddb0a
minuyim <minu_yim@naver.com>
2020-08-25 16:46:46 +0900

    refactor: 테스트에서 Security가 적용되도록 수정

---
+++ back/src/test/java/com/cocktailpick/back/common/documentation/DocumentationConfig.java
@@ -0,0 +1,9 @@
+package com.cocktailpick.back.common.documentation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.cocktailpick.back.security")
+public class DocumentationConfig {
+}
