a2bd68873c208eab74062fdbfdeed3b9830e5f8d
toneyparky <qkrtmddhks95@gmail.com>
2020-09-01 10:09:44 +0900

    feat: TerminologyService, 생성 메서드 구현
    - TerminologyRepository 생성
    - Terminology 빌더에 id 추가

--- back/src/main/java/com/cocktailpick/back/dictionary/service/TerminologyService.java
+++ back/src/main/java/com/cocktailpick/back/dictionary/service/TerminologyService.java
@@ -3,14 +3,16 @@ package com.cocktailpick.back.dictionary.service;
 import org.springframework.stereotype.Service;
 
 import com.cocktailpick.back.dictionary.domain.Terminology;
+import com.cocktailpick.back.dictionary.domain.TerminologyRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor(access = AccessLevel.PUBLIC)
 @Service
 public class TerminologyService {
+	private final TerminologyRepository terminologyRepository;
 
 	public Long save(Terminology terminology) {
-		return 1L;
+		return terminologyRepository.save(terminology).getId();
 	}
 }
