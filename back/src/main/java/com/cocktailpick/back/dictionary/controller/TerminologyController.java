d42ec2fc47771720754e196049e5eeee9c64c55b
toneyparky <qkrtmddhks95@gmail.com>
2020-09-03 09:50:52 +0900

    feat: TerminologyController, 삭제 메서드 구현
    - TerminologyService, delete 메서드 임시 구현

--- back/src/main/java/com/cocktailpick/back/dictionary/controller/TerminologyController.java
+++ back/src/main/java/com/cocktailpick/back/dictionary/controller/TerminologyController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,5 +51,11 @@ public class TerminologyController {
 		@PathVariable Long id) {
 		terminologyService.update(terminologyRequest.toTerminology(), id);
 		return ResponseEntity.ok().build();
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		terminologyService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
