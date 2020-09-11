2787d887237f52f071dff6ec8fb7b844b8ff20c4
ParkDooWon <pass345@naver.com>
2020-09-11 21:49:19 +0900

    feat : FavoriteCocktailIds DTO 구현

---
+++ back/src/main/java/com/cocktailpick/back/user/dto/FavoriteCocktailIds.java
@@ -0,0 +1,15 @@
+package com.cocktailpick.back.user.dto;
+
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class FavoriteCocktailIds {
+	private List<Long> ids;
+}
