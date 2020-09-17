04b0825a51fee93cddcce0eeb866f72dbef13950
toneyparky <qkrtmddhks95@gmail.com>
2020-09-17 09:01:07 +0900

    refactor: 용어 타입 테스트에서 모든 용어 자체를 검사하도록 수정

--- back/src/test/java/com/cocktailpick/back/dictionary/domain/TerminologyTypeTest.java
+++ back/src/test/java/com/cocktailpick/back/dictionary/domain/TerminologyTypeTest.java
@@ -2,16 +2,32 @@ package com.cocktailpick.back.dictionary.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import com.cocktailpick.back.common.exceptions.InvalidValueException;
 
 class TerminologyTypeTest {
+
 	@DisplayName("TerminologyType에 포함되어 있는 name인 경우 알맞은 객체를 반환한다.")
-	@Test
-	void of_WhenValidName_ReturnObject() {
-		assertThat(TerminologyType.of("술")).isInstanceOf(TerminologyType.class);
+	@MethodSource("provideTypeNameAndTerminologyType")
+	@ParameterizedTest
+	void of_WhenValidName_ReturnObject(String typeName, TerminologyType terminologyType) {
+		assertThat(TerminologyType.of(typeName)).isEqualTo(terminologyType);
+	}
+
+	private static Stream<Arguments> provideTypeNameAndTerminologyType() {
+		return Stream.of(
+			Arguments.of("칵테일", TerminologyType.COCKTAIL),
+			Arguments.of("술", TerminologyType.ALCOHOL),
+			Arguments.of("제조법", TerminologyType.METHOD),
+			Arguments.of("형태", TerminologyType.SHAPE)
+		);
 	}
 
 	@DisplayName("TerminologyType에 포함되어 있지 않은 name인 경우 예외를 던진다.")
