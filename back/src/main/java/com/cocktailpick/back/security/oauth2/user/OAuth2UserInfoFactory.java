1cbc1fb8f179c81a34354542ba4e5528ced5c59a
minuyim <minu_yim@naver.com>
2020-09-21 16:13:15 +0900

    refactor: 페이스북 소셜 로그인 기능 삭제

--- back/src/main/java/com/cocktailpick/back/security/oauth2/user/OAuth2UserInfoFactory.java
+++ back/src/main/java/com/cocktailpick/back/security/oauth2/user/OAuth2UserInfoFactory.java
@@ -9,8 +9,6 @@ public class OAuth2UserInfoFactory {
 	public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
 		if (registrationId.equalsIgnoreCase(AuthProvider.google.toString())) {
 			return new GoogleOAuth2UserInfo(attributes);
-		} else if (registrationId.equalsIgnoreCase(AuthProvider.facebook.toString())) {
-			return new FacebookOAuth2UserInfo(attributes);
 		}
 		throw new OAuth2AuthenticationProcessingException(
 			registrationId + " 로그인은 지원하지 않습니다.");
