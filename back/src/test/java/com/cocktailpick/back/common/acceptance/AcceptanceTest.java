0764a86590228df54733f2744e9deb00302e9758
kuenhwi <kueniyam@naver.com>
2020-10-21 19:39:39 +0900

    chore: test 환경을 분리한다.

--- back/src/test/java/com/cocktailpick/back/common/acceptance/AcceptanceTest.java
+++ back/src/test/java/com/cocktailpick/back/common/acceptance/AcceptanceTest.java
@@ -4,12 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.ActiveProfiles;
 
 import io.restassured.RestAssured;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles(profiles = {"local"})
 public abstract class AcceptanceTest {
     @LocalServerPort
     int port;
