19c4803a8ed3748c2c02e606d8e2f524eb194e21
minuyim <minu_yim@naver.com>
2020-11-02 15:36:48 +0900

    fix: secure 설정 변경

--- back/cocktailpick-api/src/main/java/com/cocktailpick/api/util/CookieUtils.java
+++ back/cocktailpick-api/src/main/java/com/cocktailpick/api/util/CookieUtils.java
@@ -31,6 +31,7 @@ public class CookieUtils {
 	public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
 		Cookie cookie = new Cookie(name, value);
 		cookie.setPath("/");
+		cookie.setSecure(true);
 		cookie.setHttpOnly(true);
 		cookie.setMaxAge(maxAge);
 		response.addCookie(cookie);
