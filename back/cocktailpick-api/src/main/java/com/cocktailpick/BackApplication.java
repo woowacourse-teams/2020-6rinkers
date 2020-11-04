9c4f67dfa39387ff8e503a82a9b37136b0d6d41f
ParkDooWon <pass345@naver.com>
2020-11-04 17:12:39 +0900

    refactor : 캐싱 설정 파일 생성

--- back/cocktailpick-api/src/main/java/com/cocktailpick/BackApplication.java
+++ back/cocktailpick-api/src/main/java/com/cocktailpick/BackApplication.java
@@ -3,13 +3,11 @@ package com.cocktailpick;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cache.annotation.EnableCaching;
 
 import com.cocktailpick.api.config.security.AppProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
-@EnableCaching
 public class BackApplication {
 
 	public static void main(String[] args) {
