dab68afdd5efa6a5cd0b9831a06d404cd70b6c0c
toneyparky <qkrtmddhks95@gmail.com>
2021-01-29 16:44:00 +0900

    style: 불필요한 코드 제거

--- back/deploy.sh
+++ back/deploy.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
 \cp -f /home/ubuntu/docker/jdk/jar/cocktailpick-api-1.0.jar /home/ubuntu/docker/jdk/back.jar
-
-#cd ..
-#
-#docker rm $(docker container ps -a -q --filter ancestor=back)
-#
-#docker run -d --name jdk back
