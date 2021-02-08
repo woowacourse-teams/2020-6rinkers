6a9090c94b232490efce7cba42ee86fc2e59e9f0
toneyparky <qkrtmddhks95@gmail.com>
2021-02-08 20:35:52 +0900

    feat: IngredientResponse에 id 추가

--- back/cocktailpick-core/src/main/java/com/cocktailpick/core/ingredient/dto/IngredientResponse.java
+++ back/cocktailpick-core/src/main/java/com/cocktailpick/core/ingredient/dto/IngredientResponse.java
@@ -1,38 +1,42 @@
 package com.cocktailpick.core.ingredient.dto;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.cocktailpick.core.ingredient.domain.Ingredient;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class IngredientResponse {
+    private Long id;
     private String title;
     private String color;
     private Double abv;
 
     @Builder
-    public IngredientResponse(String title, String color, Double abv) {
+    public IngredientResponse(Long id, String title, String color, Double abv) {
+        this.id = id;
         this.title = title;
         this.color = color;
         this.abv = abv;
     }
 
-    public static IngredientResponse of(Ingredient ingredient){
+    public static IngredientResponse of(Ingredient ingredient) {
         return IngredientResponse.builder()
-                .title(ingredient.getTitle())
-                .abv(ingredient.getAbv())
-                .color(ingredient.getColor())
-                .build();
+            .id(ingredient.getId())
+            .title(ingredient.getTitle())
+            .abv(ingredient.getAbv())
+            .color(ingredient.getColor())
+            .build();
     }
+
     public static List<IngredientResponse> listOf(List<Ingredient> ingredients) {
         return ingredients.stream()
-                .map(IngredientResponse::of)
-                .collect(Collectors.toList());
+            .map(IngredientResponse::of)
+            .collect(Collectors.toList());
     }
 }
