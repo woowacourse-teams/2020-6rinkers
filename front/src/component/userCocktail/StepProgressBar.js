4dea9dc5a878ec69a6ae8186e247daa58d9e10bc
toneyparky <qkrtmddhks95@gmail.com>
2021-02-15 15:45:14 +0900

    feat: StepProgressBar 컴포넌트 추가
    - 외부 모듈 사용

---
+++ front/src/component/userCocktail/StepProgressBar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { ProgressBar, Step } from "react-step-progress-bar";
+
+const StepProgressBar = ({ percent }) => {
+  return (
+    <ProgressBar className="step-progress-bar" percent={percent}>
+      <Step>
+        {({ accomplished, index }) => (
+          <div
+            className={`indexedStep ${accomplished ? "accomplished" : null}`}
+          >
+            {index + 1}
+          </div>
+        )}
+      </Step>
+      <Step>
+        {({ accomplished, index }) => (
+          <div
+            className={`indexedStep ${accomplished ? "accomplished" : null}`}
+          >
+            {index + 1}
+          </div>
+        )}
+      </Step>
+      <Step>
+        {({ accomplished, index }) => (
+          <div
+            className={`indexedStep ${accomplished ? "accomplished" : null}`}
+          >
+            {index + 1}
+          </div>
+        )}
+      </Step>
+      <Step>
+        {({ accomplished, index }) => (
+          <div
+            className={`indexedStep ${accomplished ? "accomplished" : null}`}
+          >
+            {index + 1}
+          </div>
+        )}
+      </Step>
+    </ProgressBar>
+  );
+};
+
+export default StepProgressBar;
