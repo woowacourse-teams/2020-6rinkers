ded9901e57d190b6a72799e051656130123eac53
toneyparky <qkrtmddhks95@gmail.com>
2021-02-15 16:24:51 +0900

    feat: url path에 따라 다른 컴포넌트를 보여주도록 설정
    - Name 컴포넌트 생성

---
+++ front/src/component/userCocktail/Name.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Name = (props) => {
+  return <div>name 화면입니다.</div>;
+};
+
+export default Name;
--- front/src/component/userCocktail/UserCocktail.js
+++ front/src/component/userCocktail/UserCocktail.js
@@ -3,12 +3,34 @@ import StepProgressBar from "./StepProgressBar";
 import "react-step-progress-bar/styles.css";
 import "../../css/userCocktail/userCocktail.css";
 import Question from "./Question";
+import Name from "./Name";
 
 const UserCocktail = () => {
+  const currentPath = window.location.pathname.replace("/my-cocktail/", "");
+  const INITIAL_PATH = "";
+
+  const renderContents = () => {
+    switch (currentPath) {
+      case INITIAL_PATH:
+        return <div> 처음 화면이니까 </div>;
+      case "name":
+        return <Name />;
+      case "ingredients":
+        return <div> ingredients 화면이니까 </div>;
+      case "glass":
+        return <div> glass 화면이니까 </div>;
+      case "amount":
+        return <div> amount 화면이니까 </div>;
+      case "recipe":
+        return <div> recipe 화면이니까 </div>;
+    }
+  };
+
   return (
     <div className="user-cocktail">
-      <StepProgressBar percent={"34"} />
+      {currentPath !== INITIAL_PATH && <StepProgressBar percent={"34"} />}
       <Question stage="name" />
+      {renderContents()}
     </div>
   );
 };
