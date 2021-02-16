a055f63bfdc4974a4e96084e04aef91540893d22
toneyparky <qkrtmddhks95@gmail.com>
2021-02-16 22:17:47 +0900

    feat: user cocktail에 대한 recoil atom 추가

--- front/src/constants/index.js
+++ front/src/constants/index.js
@@ -31,7 +31,19 @@ export const INGREDIENT_ADMIN_PROTOTYPE = {
   title: "",
   color: "",
   abv: "",
-}
+};
+
+export const USER_COCKTAIL_PROTOTYPE = {
+  name: "",
+  recipe: [
+    {
+      ingredient: "",
+      glass: "",
+      amount: "",
+    },
+  ],
+  description: "",
+};
 
 export const ACCESS_TOKEN = "accessToken";
 export const API_BASE_URL = `//${process.env.REACT_APP_HOST}`;
