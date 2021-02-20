b08f4e1399ea08cc596c1f8cf0949cfa3bd7c74b
toneyparky <qkrtmddhks95@gmail.com>
2021-02-20 23:39:31 +0900

    refactor: for를 이용하여 중복 제거

--- front/src/component/userCocktail/StepProgressBar.js
+++ front/src/component/userCocktail/StepProgressBar.js
@@ -2,43 +2,25 @@ import React from "react";
 import { ProgressBar, Step } from "react-step-progress-bar";
 
 const StepProgressBar = ({ percent }) => {
+  const count = 5;
+
+  const steps = [];
+
+  for (let i = 0; i < count; i++) {
+    steps.push(
+      <Step>
+        {({ accomplished }) => (
+          <div className={`indexed-step ${accomplished ? "accomplished" : ""}`}>
+            {i + 1}
+          </div>
+        )}
+      </Step>
+    );
+  }
+
   return (
     <ProgressBar className="step-progress-bar" percent={percent}>
-      <Step>
-        {({ accomplished, index }) => (
-          <div className={`indexed-step ${accomplished ? "accomplished" : ""}`}>
-            {index + 1}
-          </div>
-        )}
-      </Step>
-      <Step>
-        {({ accomplished, index }) => (
-          <div className={`indexed-step ${accomplished ? "accomplished" : ""}`}>
-            {index + 1}
-          </div>
-        )}
-      </Step>
-      <Step>
-        {({ accomplished, index }) => (
-          <div className={`indexed-step ${accomplished ? "accomplished" : ""}`}>
-            {index + 1}
-          </div>
-        )}
-      </Step>
-      <Step>
-        {({ accomplished, index }) => (
-          <div className={`indexed-step ${accomplished ? "accomplished" : ""}`}>
-            {index + 1}
-          </div>
-        )}
-      </Step>
-      <Step>
-        {({ accomplished, index }) => (
-          <div className={`indexed-step ${accomplished ? "accomplished" : ""}`}>
-            {index + 1}
-          </div>
-        )}
-      </Step>
+      {steps}
     </ProgressBar>
   );
 };
