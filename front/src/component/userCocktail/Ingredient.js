6873e88ece2a28370874c41b63e97662916ab876
toneyparky <qkrtmddhks95@gmail.com>
2021-03-01 16:57:44 +0900

    feat: api를 이용하여 재료 조회 후 화면에 표시

--- front/src/component/userCocktail/Ingredient.js
+++ front/src/component/userCocktail/Ingredient.js
@@ -1,18 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { userCocktailState } from "../../recoil";
 import { useHistory } from "react-router-dom";
+import { fetchAllIngredients } from "../../api";
 
 const Ingredient = ({ setStage }) => {
   const history = useHistory();
   const [userCocktail, setUserCocktail] = useRecoilState(userCocktailState);
   const [selected, setSelected] = useState({ id: 0, name: "기본" });
+  const [ingredients, setIngredients] = useState([]);
 
   const searchIngredient = (e) => {
     // 자동완성 api 쏘고 받아오고 띄워주고. 그니가 구현한 자동완성처럼.
     // 혹은 아래 ingredients를 받아올 때에 모든 ingredients를 다 받아오고 랜덤으로 몇 개만 아래에서 보여주고
     // 여기서 자동완성도 api 안찌르고 미리 받아온거 내에서 처리하기.
   };
+
+  const fetchIngredients = async () => {
+    const response = await fetchAllIngredients();
+    setIngredients(response.data);
+  };
+
+  useEffect(() => {
+    fetchIngredients();
+  }, []);
 
   const onNext = (e) => {
     e.preventDefault();
@@ -27,25 +38,9 @@ const Ingredient = ({ setStage }) => {
     setStage("quantity-unit");
   };
 
-  const ingredients = () => {
-    // api를 쏴서 재료 리스트를 받아온다.
-    return [
-      { id: 1, name: "소주" },
-      { id: 2, name: "맥주" },
-      { id: 3, name: "탕수육" },
-      { id: 4, name: "짜장면" },
-      { id: 5, name: "민트" },
-      { id: 6, name: "게토레이" },
-      { id: 7, name: "사이다" },
-      { id: 8, name: "생크림" },
-      { id: 9, name: "김치찌개" },
-      { id: 10, name: "수육" },
-    ];
-  };
-
   const onSelect = (e) => {
     const selectedId = e.target.dataset.id;
-    const found = ingredients().find((it) => it.id === parseInt(selectedId));
+    const found = ingredients.find((it) => it.id === parseInt(selectedId));
     setSelected(found);
   };
 
@@ -58,13 +53,18 @@ const Ingredient = ({ setStage }) => {
       </div>
       <input type="text" placeholder="검색해보세요." />
       <div>
-        {ingredients().map((it, index) => {
-          return (
-            <div key={"ingredient" + index} data-id={it.id} onClick={onSelect}>
-              {it.name}
-            </div>
-          );
-        })}
+        {ingredients &&
+          ingredients.map((it, index) => {
+            return (
+              <div
+                key={"ingredient" + index}
+                data-id={it.id}
+                onClick={onSelect}
+              >
+                {it.name}
+              </div>
+            );
+          })}
       </div>
       <button className="name-button" type="submit" onClick={onNext}>
         다음 단계로 가보죠
