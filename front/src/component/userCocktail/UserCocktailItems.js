974b2944e31621eaadd1153d2d5184ba99036fdb
toneyparky <qkrtmddhks95@gmail.com>
2021-03-04 13:45:17 +0900

    feat: quantity와 재료의 색상에 따라 칵테일의 색상을 생성

--- front/src/component/userCocktail/UserCocktailItems.js
+++ front/src/component/userCocktail/UserCocktailItems.js
@@ -3,6 +3,7 @@ import { useRecoilState } from "recoil";
 import { userCocktailState } from "../../recoil";
 import { useHistory } from "react-router-dom";
 import { fetchAllUserCocktails } from "../../api";
+import mix from "mix-css-color";
 
 const UserCocktailItems = ({ setStage }) => {
   // todo 사실은 api를 쏴서 보여줘야 한다.
@@ -27,11 +28,39 @@ const UserCocktailItems = ({ setStage }) => {
     setStage("name");
   };
 
+  const mixColor = (cocktail) => {
+    const items = cocktail.userRecipeItemResponses;
+    let quantity = items[0].quantity;
+    let mixedColor = items[0].ingredientColor;
+    for (let i = 1; i < items.length; i++) {
+      const tempQuantity = (
+        (quantity / (quantity + items[i].quantity)) *
+        100
+      ).toFixed(0);
+      mixedColor = mix(
+        items[i - 1].ingredientColor,
+        items[i].ingredientColor,
+        tempQuantity
+      );
+      quantity = tempQuantity;
+    }
+    try {
+      return `rgba(${mixedColor.rgba[0]}, ${mixedColor.rgba[1]}, ${mixedColor.rgba[2]}, ${mixedColor.rgba[3]})`;
+    } catch (err) {
+      return "rgba(0,0,0,0)";
+    }
+  };
+
   return (
     <>
       {userCocktails &&
         userCocktails.map((it, index) => (
-          <div key={`user-cocktail-${index}`}>{it.name}</div>
+          <div
+            key={`user-cocktail-${index}`}
+            style={{ backgroundColor: `${mixColor(it)}` }}
+          >
+            {it.name}
+          </div>
         ))}
       <button onClick={onCreate}>나도 만들기</button>
     </>
