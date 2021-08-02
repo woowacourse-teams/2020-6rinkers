8135a6f09a2787c67ba1fb5ac57106e5c7e97862
toneyparky <qkrtmddhks95@gmail.com>
2021-08-02 23:16:49 +0900

    fix: google oauth

--- front/src/constants/index.js
+++ front/src/constants/index.js
@@ -53,4 +53,4 @@ export const USER_COCKTAIL_PROTOTYPE = {
 export const ACCESS_TOKEN = "accessToken";
 export const API_BASE_URL = `//${process.env.REACT_APP_HOST}`;
 export const OAUTH2_REDIRECT_URI = `${API_BASE_URL}/oauth2/redirect`;
-export const GOOGLE_AUTH_URL = `${API_BASE_URL}/api/oauth2/authorize/google?redirect_uri=http://localhost:3000/oauth2/redirect`;
+export const GOOGLE_AUTH_URL = `${API_BASE_URL}/api/oauth2/authorize/google?redirect_uri=${OAUTH2_REDIRECT_URI}`;
