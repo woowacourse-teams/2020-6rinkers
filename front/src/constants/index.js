669e7618c97199c968a36a46480cf3711f4a2c9c
toneyparky <qkrtmddhks95@gmail.com>
2021-08-02 23:52:04 +0900

    fix: fix oauth url

--- front/src/constants/index.js
+++ front/src/constants/index.js
@@ -53,4 +53,4 @@ export const USER_COCKTAIL_PROTOTYPE = {
 export const ACCESS_TOKEN = "accessToken";
 export const API_BASE_URL = `//${process.env.REACT_APP_HOST}`;
 export const OAUTH2_REDIRECT_URI = `${API_BASE_URL}/oauth2/redirect`;
-export const GOOGLE_AUTH_URL = `${API_BASE_URL}/api/oauth2/authorize/google?redirect_uri=https://www.cocktailpick.com/oauth2/redirect`;
+export const GOOGLE_AUTH_URL = `https://www.cocktailpick.com/api/oauth2/authorize/google?redirect_uri=https://www.cocktailpick.com/oauth2/redirect`;
