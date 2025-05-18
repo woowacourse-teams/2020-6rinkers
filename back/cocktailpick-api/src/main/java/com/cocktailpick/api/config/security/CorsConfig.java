44119d56deaea9d3b0a64b80ae19b14a216b6b77
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-19 07:26:11 +0900

    feat: 스프링 시큐리티 필터에서 사용할 cors 설정 빈

---
+++ back/cocktailpick-api/src/main/java/com/cocktailpick/api/config/security/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.cocktailpick.api.config.security;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    @Qualifier("SecurityFilterCors")
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
