16113c43e65b1aa4300dfad78dff4a36e5ff05f1
jeongseonwoong <tjsdnd1219@naver.com>
2025-06-25 14:23:28 +0900

    feat: 인증 예외 발생 시 사용되는 클래스 빈으로 등록 후 사용하도록 변경

--- back/cocktailpick-api/src/main/java/com/cocktailpick/api/config/security/SecurityConfig.java
+++ back/cocktailpick-api/src/main/java/com/cocktailpick/api/config/security/SecurityConfig.java
@@ -6,8 +6,6 @@ import com.cocktailpick.api.security.oauth2.HttpCookieOAuth2AuthorizationRequest
 import com.cocktailpick.api.security.oauth2.OAuth2AuthenticationFailureHandler;
 import com.cocktailpick.api.security.oauth2.OAuth2AuthenticationSuccessHandler;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -31,39 +29,41 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
-public class SecurityConfig{
-	private static final String ADMIN = "ADMIN";
-	private static final String USER = "USER";
+public class SecurityConfig {
+    private static final String ADMIN = "ADMIN";
+    private static final String USER = "USER";
 
-	private final CustomUserDetailsService customUserDetailsService;
+    private final CustomUserDetailsService customUserDetailsService;
 
-	private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
-	private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
 
-	private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+    private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
 
-	private final TokenProvider tokenProvider;
+    private final TokenProvider tokenProvider;
 
-	private final TokenAuthenticationFilter tokenAuthenticationFilter;
+    private final TokenAuthenticationFilter tokenAuthenticationFilter;
 
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.addAllowedOriginPattern("*");
-		configuration.addAllowedHeader("*");
-		configuration.addAllowedMethod("*");
-		configuration.setAllowCredentials(true);
+    private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
 
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
 
-	@Bean
-	public HttpCookieOAuth2AuthorizationRequestRepository cookieAuthorizationRequestRepository() {
-		return new HttpCookieOAuth2AuthorizationRequestRepository();
-	}
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public HttpCookieOAuth2AuthorizationRequestRepository cookieAuthorizationRequestRepository() {
+        return new HttpCookieOAuth2AuthorizationRequestRepository();
+    }
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
@@ -73,97 +73,97 @@ public class SecurityConfig{
         return daoAuthenticationProvider;
     }
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean(BeanIds.AUTHENTICATION_MANAGER)
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
-		http
-            .headers(h -> h.frameOptions().disable())
-			.cors(c -> c.configurationSource(corsConfigurationSource()))
-			.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-			.csrf(csrf ->csrf.disable())
-			.formLogin(fl ->fl.disable())
-			.httpBasic(hb -> hb.disable())
-			.exceptionHandling(eh ->eh.authenticationEntryPoint(new RestAuthenticationEntryPoint()))
-            .authorizeRequests(auth -> auth
-                    .antMatchers("/",
-                            "/error",
-                            "/favicon.ico",
-                            "/**/*.png",
-                            "/**/*.gif",
-                            "/**/*.svg",
-                            "/**/*.jpg",
-                            "/**/*.html",
-                            "/**/*.css",
-                            "/**/*.js")
-                    .permitAll()
-                    .antMatchers(HttpMethod.POST, "/**/upload/csv")
-                    .hasRole(ADMIN)
-                    .antMatchers(HttpMethod.POST, "/api/cocktails")
-                    .hasRole(ADMIN)
-                    .antMatchers(HttpMethod.PUT, "/api/cocktails/**")
-                    .hasRole(ADMIN)
-                    .antMatchers(HttpMethod.DELETE, "/api/cocktails/**")
-                    .hasRole(ADMIN)
-                    .antMatchers(HttpMethod.POST, "/api/tags")
-                    .hasRole(ADMIN)
-                    .antMatchers(HttpMethod.PUT, "/api/tags/**")
-                    .hasRole(ADMIN)
-                    .antMatchers(HttpMethod.DELETE, "/api/tags/**")
-                    .hasRole(ADMIN)
-                    .antMatchers(HttpMethod.POST, "/api/terminologies")
-                    .hasRole(ADMIN)
-                    .antMatchers(HttpMethod.PUT, "/api/terminologies/**")
-                    .hasRole(ADMIN)
-                    .antMatchers(HttpMethod.DELETE, "/api/terminologies/**")
-                    .hasRole(ADMIN)
-                    .antMatchers(HttpMethod.GET, "/api/ingredients/**")
-                    .hasAnyRole(USER, ADMIN)
-                    .antMatchers(HttpMethod.POST, "/api/ingredients")
-                    .hasRole(ADMIN)
-                    .antMatchers(HttpMethod.PUT, "/api/ingredients/**")
-                    .hasRole(ADMIN)
-                    .antMatchers(HttpMethod.DELETE, "/api/ingredients/**")
-                    .hasRole(ADMIN)
-                    .antMatchers(HttpMethod.POST, "/api/user-cocktails/**")
-                    .hasAnyRole(USER, ADMIN)
-                    .antMatchers(HttpMethod.GET, "/api/user-cocktails/**")
-                    .permitAll()
-                    .antMatchers(HttpMethod.PUT, "/api/user-cocktails/**")
-                    .hasAnyRole(USER, ADMIN)
-                    .anyRequest()
-                    .permitAll()
-            )
-            .oauth2Login(oauth -> oauth
-                    .authorizationEndpoint(aep -> aep
-                            .baseUri("/api/oauth2/authorize")
-                            .authorizationRequestRepository(cookieAuthorizationRequestRepository()))
-                    .redirectionEndpoint(rep -> rep
-                            .baseUri("/api/oauth2/callback/*"))
-                    .userInfoEndpoint(uep -> uep.userService(customOAuth2UserService))
-                    .successHandler(oAuth2AuthenticationSuccessHandler)
-                    .failureHandler(oAuth2AuthenticationFailureHandler)
-            );
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+        http
+                .headers(h -> h.frameOptions().disable())
+                .cors(c -> c.configurationSource(corsConfigurationSource()))
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable())
+                .formLogin(fl -> fl.disable())
+                .httpBasic(hb -> hb.disable())
+                .exceptionHandling(eh -> eh.authenticationEntryPoint(restAuthenticationEntryPoint))
+                .authorizeRequests(auth -> auth
+                        .antMatchers("/",
+                                "/error",
+                                "/favicon.ico",
+                                "/**/*.png",
+                                "/**/*.gif",
+                                "/**/*.svg",
+                                "/**/*.jpg",
+                                "/**/*.html",
+                                "/**/*.css",
+                                "/**/*.js")
+                        .permitAll()
+                        .antMatchers(HttpMethod.POST, "/**/upload/csv")
+                        .hasRole(ADMIN)
+                        .antMatchers(HttpMethod.POST, "/api/cocktails")
+                        .hasRole(ADMIN)
+                        .antMatchers(HttpMethod.PUT, "/api/cocktails/**")
+                        .hasRole(ADMIN)
+                        .antMatchers(HttpMethod.DELETE, "/api/cocktails/**")
+                        .hasRole(ADMIN)
+                        .antMatchers(HttpMethod.POST, "/api/tags")
+                        .hasRole(ADMIN)
+                        .antMatchers(HttpMethod.PUT, "/api/tags/**")
+                        .hasRole(ADMIN)
+                        .antMatchers(HttpMethod.DELETE, "/api/tags/**")
+                        .hasRole(ADMIN)
+                        .antMatchers(HttpMethod.POST, "/api/terminologies")
+                        .hasRole(ADMIN)
+                        .antMatchers(HttpMethod.PUT, "/api/terminologies/**")
+                        .hasRole(ADMIN)
+                        .antMatchers(HttpMethod.DELETE, "/api/terminologies/**")
+                        .hasRole(ADMIN)
+                        .antMatchers(HttpMethod.GET, "/api/ingredients/**")
+                        .hasAnyRole(USER, ADMIN)
+                        .antMatchers(HttpMethod.POST, "/api/ingredients")
+                        .hasRole(ADMIN)
+                        .antMatchers(HttpMethod.PUT, "/api/ingredients/**")
+                        .hasRole(ADMIN)
+                        .antMatchers(HttpMethod.DELETE, "/api/ingredients/**")
+                        .hasRole(ADMIN)
+                        .antMatchers(HttpMethod.POST, "/api/user-cocktails/**")
+                        .hasAnyRole(USER, ADMIN)
+                        .antMatchers(HttpMethod.GET, "/api/user-cocktails/**")
+                        .permitAll()
+                        .antMatchers(HttpMethod.PUT, "/api/user-cocktails/**")
+                        .hasAnyRole(USER, ADMIN)
+                        .anyRequest()
+                        .permitAll()
+                )
+                .oauth2Login(oauth -> oauth
+                        .authorizationEndpoint(aep -> aep
+                                .baseUri("/api/oauth2/authorize")
+                                .authorizationRequestRepository(cookieAuthorizationRequestRepository()))
+                        .redirectionEndpoint(rep -> rep
+                                .baseUri("/api/oauth2/callback/*"))
+                        .userInfoEndpoint(uep -> uep.userService(customOAuth2UserService))
+                        .successHandler(oAuth2AuthenticationSuccessHandler)
+                        .failureHandler(oAuth2AuthenticationFailureHandler)
+                );
 
-		http.addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-		http.addFilterBefore(loginFilter(authenticationManager), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(loginFilter(authenticationManager), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
-	}
+    }
 
-	private LoginFilter loginFilter(AuthenticationManager authenticationManager) throws Exception {
-		LoginFilter loginFilter = new LoginFilter();
-		loginFilter.setFilterProcessesUrl("/api/user/login");
-		loginFilter.setAuthenticationManager(authenticationManager);
-		loginFilter.setAuthenticationSuccessHandler(new LoginSuccessHandler(tokenProvider));
-		return loginFilter;
-	}
+    private LoginFilter loginFilter(AuthenticationManager authenticationManager) throws Exception {
+        LoginFilter loginFilter = new LoginFilter();
+        loginFilter.setFilterProcessesUrl("/api/user/login");
+        loginFilter.setAuthenticationManager(authenticationManager);
+        loginFilter.setAuthenticationSuccessHandler(new LoginSuccessHandler(tokenProvider));
+        return loginFilter;
+    }
 }
--- back/cocktailpick-api/src/main/java/com/cocktailpick/api/security/RestAuthenticationEntryPoint.java
+++ back/cocktailpick-api/src/main/java/com/cocktailpick/api/security/RestAuthenticationEntryPoint.java
@@ -1,24 +1,24 @@
 package com.cocktailpick.api.security;
 
-import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
-
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
 
 @Slf4j
+@Component
 public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-	@Override
-	public void commence(HttpServletRequest httpServletRequest,
-		HttpServletResponse httpServletResponse,
-		AuthenticationException e) throws IOException {
-		log.info("Responding with unauthorized error. Message - {}", e.getMessage());
-		httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED,
-			e.getLocalizedMessage());
-	}
+    @Override
+    public void commence(HttpServletRequest httpServletRequest,
+                         HttpServletResponse httpServletResponse,
+                         AuthenticationException e) throws IOException {
+        log.info("Responding with unauthorized error. Message - {}", e.getMessage());
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+                e.getLocalizedMessage());
+    }
 }
